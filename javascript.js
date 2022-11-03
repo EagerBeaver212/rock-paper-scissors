237f577f365320c0ba88fdd6d26e2297c1560817
Greg C <gregcourt10@gmail.com>
2022-11-03 19:41:11 +0000

    console-only draft of rps game complete

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
-alert("Hello Bro!")
+let sampleSpace = ["rock", "paper", "scissors"]
+
+function getComputerChoice(items) {
+  return items[Math.floor(Math.random()*sampleSpace.length)];
+}
+
+function oneRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection)
+    return ("Tie!")
+
+  else if (playerSelection === "rock" && computerSelection === "paper")
+    return ("You Lose! Paper beats Rock!")
+
+  else if (playerSelection === "rock" && computerSelection === "scissors")
+    return ("You Win! Rock beats Scissors!")
+
+  else if (playerSelection === "paper" && computerSelection === "rock")
+    return ("You Win! Paper beats Rock!") 
+
+  else if (playerSelection === "paper" && computerSelection === "scissors")
+    return ("You Lose! Scissors beats Paper!") 
+
+  else if (playerSelection === "scissors" && computerSelection === "paper")
+    return ("You Win! Scissors beats Paper!") 
+
+  else if (playerSelection === "scissors" && computerSelection === "rock")
+    return ("You Lose! Rock beats Scissors!") 
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = (prompt("What hand?")).toLowerCase();
+    let computerSelection = getComputerChoice(sampleSpace);
+    oneRound(playerSelection, computerSelection)
+    console.log(oneRound(playerSelection, computerSelection))
+  }
+}
+
+game()
