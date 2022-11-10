6e479b76edba242f4dcc795d10f9e6a2c22b70c1
Greg <gregcourt10@gmail.com>
2022-11-10 14:33:27 +0000

    rps-ui v1

--- javascript.js
+++ javascript.js
@@ -1,46 +1,88 @@
 let sampleSpace = ["rock", "paper", "scissors"]
 
 function getComputerChoice(items) {
-  return items[Math.floor(Math.random()*sampleSpace.length)];
+  return items[Math.floor(Math.random()*items.length)];
 }
+
+let computerScore = 0
+let playerScore = 0
 
 function oneRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection)
     return ("Tie!")
 
-  else if (playerSelection === "rock" && computerSelection === "paper")
+  else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore += 1
     return ("You Lose! Paper beats Rock!")
+  }
 
-  else if (playerSelection === "rock" && computerSelection === "scissors")
+  else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore += 1
     return ("You Win! Rock beats Scissors!")
+  }
 
-  else if (playerSelection === "paper" && computerSelection === "rock")
-    return ("You Win! Paper beats Rock!") 
+  else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore += 1
+    return ("You Win! Paper beats Rock!")
+  }
 
-  else if (playerSelection === "paper" && computerSelection === "scissors")
-    return ("You Lose! Scissors beats Paper!") 
+  else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore += 1
+    return ("You Lose! Scissors beats Paper!")
+  }
 
-  else if (playerSelection === "scissors" && computerSelection === "paper")
-    return ("You Win! Scissors beats Paper!") 
+  else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore += 1
+    return ("You Win! Scissors beats Paper!")
+  }
 
-  else if (playerSelection === "scissors" && computerSelection === "rock")
+  else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore += 1
     return ("You Lose! Rock beats Scissors!") 
+  }
 }
-
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     let playerSelection = (prompt("What hand?")).toLowerCase();
-//     let computerSelection = getComputerChoice(sampleSpace);
-//     oneRound(playerSelection, computerSelection)
-//     console.log(oneRound(playerSelection, computerSelection))
-//   }
-// }
-// game()
 
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
 
-rock.addEventListener('click', () => { console.log(oneRound("rock", getComputerChoice(sampleSpace))) })
-paper.addEventListener('click', () => { console.log(oneRound("paper", getComputerChoice(sampleSpace))) })
-scissors.addEventListener('click', () => { console.log(oneRound("scissors", getComputerChoice(sampleSpace))) })
+let results = document.querySelector(".results")
+let playerDisplayScore = document.querySelector(".player-score")
+let computerDisplayScore = document.querySelector(".computer-score")
+
+function updatePlayer() { playerDisplayScore.textContent = `Player Score: ${playerScore}` }
+function updateComputer() { computerDisplayScore.textContent = `Computer Score: ${computerScore}` }
+updatePlayer()
+updateComputer()
+
+function game() {
+  results.innerHTML = "Pick Rock, Paper or Scissors!"
+  rock.addEventListener('click', () => { 
+    let roundResult = oneRound("rock", getComputerChoice(sampleSpace))
+    results.innerHTML = (roundResult)
+    updatePlayer()
+    updateComputer()
+  })
+  
+  paper.addEventListener('click', () => { 
+    let roundResult = oneRound("paper", getComputerChoice(sampleSpace))
+    results.innerHTML = (roundResult)
+    updatePlayer()
+    updateComputer()
+  })
+  
+  scissors.addEventListener('click', () => { 
+    let roundResult = oneRound("scissors", getComputerChoice(sampleSpace))
+    results.innerHTML = (roundResult)
+    updatePlayer()
+    updateComputer()
+  })
+  if (computerScore === 5) {
+    results.innerHTML = "You lost this game. Better luck next time!"
+  }
+  if (playerScore === 5) {
+    results.innerHTML = "Well done! You won this game"
+  }
+}
+
+game()
